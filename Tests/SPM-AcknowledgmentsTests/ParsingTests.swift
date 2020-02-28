91f72134675d5e99a1caa4a9507691ce1a9ebae8
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-02-27 20:15:09 -0800

    Add Unit test structure

---
+++ Tests/SPM-AcknowledgmentsTests/ParsingTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SPM_Acknowledgments
+
+final class ParsingTests: XCTestCase {
+    func testParsing() {
+		// Cannot be tested until resources support is added
+    }
+
+    static var allTests = [
+        ("testParsing", testParsing),
+    ]
+}
--- Tests/SPM-AcknowledgmentsTests/XCTestManifests.swift
+++ Tests/SPM-AcknowledgmentsTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SPM_AcknowledgmentsTests.allTests),
+        testCase(ParsingTests.allTests),
     ]
 }
 #endif
